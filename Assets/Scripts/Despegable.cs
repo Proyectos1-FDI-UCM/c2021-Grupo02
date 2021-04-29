712ca8cdf144ce5277c0f7a70b5326c998fa3a07
PabloGarciaMoreno <pablog30@ucm.es>
2021-04-29 20:26:33 +0200

    Menu de Pausa

---
+++ Assets/Scripts/Despegable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Despegable : MonoBehaviour
+{
+    public Animator animboton;
+    private Boton boton;
+   
+    // Start is called before the first frame update
+    //segun si es false o true hacer la animacion.
+    void Start()
+    {
+        animboton = GetComponent<Animator>();
+        boton = GameObject.Find("PosicionABuscar").GetComponent<Boton>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(boton.showMenu)
+        {
+            animboton.SetBool("Desplegarse", true);
+        }
+        if (boton.showMenu==false)
+        {
+            animboton.SetBool("Desplegarse", false);
+        }
+    }
+}
