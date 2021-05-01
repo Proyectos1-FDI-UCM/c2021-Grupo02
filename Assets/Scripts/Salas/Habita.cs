e7b9c94a0721e420f50bfb6ce4e42431b84638d2
MIRIAMMARTIN <mirima10@ucm.es>
2021-05-01 11:54:26 +0200

    Arregladas las salas

--- Assets/Scripts/Salas/Habita.cs
+++ Assets/Scripts/Salas/Habita.cs
@@ -5,39 +5,34 @@ using UnityEngine;
 public class Habita : MonoBehaviour
 {
     public GameObject puerta1, puerta2;
+  
     void Start()
     {
-        
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-      //  if(collision.gameObject.GetComponent<Guardia>() && collision.gameObject.GetComponent<RobotPoliciaMovimiento>())
-        //{
-            
-        //}
+      
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.GetComponent<RobotPoliciaMovimiento>()|| collision.gameObject.GetComponent<Guardia>())
-        GameManager.GetInstance().AddEnemy();
-        
-
-        if (collision.gameObject.GetComponent<ControlesPlayer>())    //Solo se produce si el objeto que entra en el trigger es el jugador
+      
+        if (collision.GetComponent<ControlesPlayer>())
         {
+            gameObject.layer = 0;
             GameManager.GetInstance().EntrarSala();
-            Invoke("DoorClosed",1);
-            
+            Invoke("DoorClosed", 1);   
         }
-       
-       
+        if (collision.GetComponent<RobotPoliciaMovimiento>()|| collision.gameObject.GetComponent<Guardia>())
+        {
+            GameManager.GetInstance().AddEnemy();
+        }
+      
     }
+
     void DoorClosed()
     {
         puerta1.SetActive(true);
     }
 
 
-private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
        
         if (GameManager.GetInstance().Enemiesmuertos())    //Solo se produce si el objeto que entra en el trigger es el jugador
