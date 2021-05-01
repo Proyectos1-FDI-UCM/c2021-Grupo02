5b257ee678e440f3f2c62eb61c241c1aabc7f46e
jacall02 <82378764+jacall02@users.noreply.github.com>
2021-05-01 12:49:23 +0200

    Revertidos los cambios desde la versión de Dani

---
+++ Assets/Boton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton : MonoBehaviour
+{ public bool showMenu;
+    // Start is called before the first frame update
+    //para cambiar variable a falso¡e o true.
+   public void ButtonShowMenu()
+    {
+        if (!showMenu) showMenu = true;
+        
+        else if (showMenu) showMenu = false;
+
+    }
+}
--- Assets/Scripts/Despegable.cs
+++ Assets/Scripts/Despegable.cs
@@ -3,51 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Despegable : MonoBehaviour
-{     
-     Animator animboton;
-    [SerializeField ]
-     static bool GameIsPaused = true;//por defecto
-    private int contador = 0;
+{
+    public Animator animboton;
+    private Boton boton;
    
     // Start is called before the first frame update
     //segun si es false o true hacer la animacion.
     void Start()
     {
         animboton = GetComponent<Animator>();
-       
-     
+        boton = GameObject.Find("Code").GetComponent<Boton>();
+        
     }
 
     // Update is called once per frame
     void Update()
-    {if(Input.GetKeyDown(KeyCode.Escape))
+    {
+        if(boton.showMenu)
         {
-            if (!GameIsPaused) //si no está pausado no queremos meter cuadro de texto
-            {
-                Resume();//esconder cuadro de texto
-
-            }
-            else//gameispaused=true , si esta pausado queremos cuadro de texto de pausa
-            {
-                Pause();
-
-            }
-        }
-        
-            
-               
-            
-    }
-     void Resume()//si esta parado que se esconda el cuadro de texto y vuelva a la normalidad , si saliendo = false , significa que tiene que esconderse el cuadro de texto
-     {
-            animboton.SetBool("Saliendo", false);
-            Time.timeScale = 1f;//lo se , es para que no se pare
-            GameIsPaused = true;
-     }
-     void Pause()
-     {
             animboton.SetBool("Saliendo", true);
-            Time.timeScale = 0f;
-            GameIsPaused = false;
-     }
+        }
+        if (boton.showMenu==false)
+        {
+            animboton.SetBool("Saliendo", false);
+        }
+    }
 }
