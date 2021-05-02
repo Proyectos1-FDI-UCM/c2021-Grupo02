52c22236152971f6783baf93be91d8bbb00c47d8
PabloGarciaMoreno <pablog30@ucm.es>
2021-05-02 22:42:33 +0200

    Version con camara con retraso para el jugador y zoom en pasillo 1

---
+++ Assets/Scripts/extras/zoomcollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class zoomcollider : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera  myVirtualCamera;
+    // Start is called before the first frame update
+   private void Start()
+    {//para iniciarse en false por defecto y que solo entre en zonas concretas
+        myVirtualCamera.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("HOLA");
+        myVirtualCamera.gameObject.SetActive(true);
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        myVirtualCamera.gameObject.SetActive(false);
+    }
+
+}
